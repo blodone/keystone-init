439d6a0da1e00d80c1d0c15ac0f568671ecb9d07
root <root@osc.ednic.net>
2015-03-08 12:25:03 +0100

    keystone check print debug infos

---
+++ keystone_check.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+
+from keystone_init import parse_config
+
+from keystoneclient.v2_0 import client
+
+
+def check_keystone(config):
+    keystone = client.Client(token=config['token'],
+                             endpoint=config['endpoint'])
+    print '{1}-{0}-{1}'.format('Endpoints', '-'*10)
+    print keystone.endpoints.list()
+    print
+
+    print '{1}-{0}-{1}'.format('Services', '-'*10)
+    print keystone.services.list()
+    print
+
+    print '{1}-{0}-{1}'.format('Users', '-'*10)
+    print keystone.users.list()
+    print 
+
+def return_keystone(config_file='config.yml'):
+    config = parse_config(config_file)
+    return check_keystone(config)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "Usage: keystone-check.py config-file.yml"
+        sys.exit(-1)
+    config = parse_config(sys.argv[1])
+    check_keystone(config)
