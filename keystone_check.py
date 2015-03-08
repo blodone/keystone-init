141646ec3cb61b157e2837343049b68f45fa57a5
root <root@osc.ednic.net>
2015-03-08 12:30:03 +0100

    keystone check management url

--- keystone_check.py
+++ keystone_check.py
@@ -9,6 +9,11 @@ from keystoneclient.v2_0 import client
 def check_keystone(config):
     keystone = client.Client(token=config['token'],
                              endpoint=config['endpoint'])
+
+    print '{1}-{0}-{1}'.format('URL', '-'*10)
+    print keystone.management_url
+    print
+
     print '{1}-{0}-{1}'.format('Endpoints', '-'*10)
     print keystone.endpoints.list()
     print
@@ -28,6 +33,9 @@ def return_keystone(config_file='config.yml'):
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print "Usage: keystone-check.py config-file.yml"
-        sys.exit(-1)
-    config = parse_config(sys.argv[1])
+	print 'using default config.yml'
+        config_file = 'config.yml'
+    else:
+	config_file = sys.argv[1]
+    config = parse_config(config_file)
     check_keystone(config)
