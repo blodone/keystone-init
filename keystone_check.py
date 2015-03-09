3affb78bb7d27d9c2203b5946988703cdbf08dee
root <root@osc.ednic.net>
2015-03-09 14:07:43 +0100

    keystone_check as list debug info

--- keystone_check.py
+++ keystone_check.py
@@ -6,6 +6,10 @@ from keystone_init import parse_config
 from keystoneclient.v2_0 import client
 
 
+def as_list(list):
+    for a in list:
+	print a
+
 def check_keystone(config):
     keystone = client.Client(token=config['token'],
                              endpoint=config['endpoint'])
@@ -15,16 +19,25 @@ def check_keystone(config):
     print
 
     print '{1}-{0}-{1}'.format('Endpoints', '-'*10)
-    print keystone.endpoints.list()
+    print as_list(keystone.endpoints.list())
     print
 
     print '{1}-{0}-{1}'.format('Services', '-'*10)
-    print keystone.services.list()
+    print as_list(keystone.services.list())
     print
 
     print '{1}-{0}-{1}'.format('Users', '-'*10)
-    print keystone.users.list()
+    print as_list(keystone.users.list())
     print 
+
+    print '{1}-{0}-{1}'.format('Roles', '-'*10)
+    print as_list(keystone.roles.list())
+    print 
+
+    print '{1}-{0}-{1}'.format('Tenants', '-'*10)
+    print as_list(keystone.tenants.list())
+    print 
+
     return keystone
 
 def return_keystone(config_file='config.yml'):
